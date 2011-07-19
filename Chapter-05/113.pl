cfd997bf7a8735546cbe75e6cc51a03edb926f41
Yunchang Kang <codenewb@gmail.com>
2011-07-19 22:55:38 +0900

    Add FileHandle

---
+++ Chapter-05/113.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+
+use 5.010;
+use utf8;
+use strict;
+use warnings;
+
+my $select_output = "my_output";
+open LOG, "> $select_output";
+
+say $select_output;
--- Chapter-05/118.pl
+++ Chapter-05/118.pl
@@ -5,9 +5,20 @@ use utf8;
 use strict;
 use warnings;
 
-if ( ! open PASSWD, "/etc/psswd" ) {
+if ( ! open PASSWD, "/etc/passwd" ) {
     die "How did you get  logged in? ($!)";
 }
+
 while (<PASSWD>) {
     chomp;
+    say;
+}
+
+if ( ! open my $passwd, "/etc/passwd" ) {
+    die "How did you get  logged in? ($!)";
+}
+
+while (<$passwd>) {
+    chomp;
+    say;
 }
